04b4e324d4cfd43b05c93f9d155422cf4540048c
Karin Ljunggren <kaljunggren@gmail.com>
2024-02-16 12:03:28 +0100

    first cat item in list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.scss";
+import Header from "./components/Header";
 
 function App() {
+  const [cats, setCats] = useState([
+    {
+      id: Math.random(),
+      catname: "Jojo",
+      goOutside: true,
+      foundHome: false,
+    },
+  ]);
+
+  useEffect(() => {}, []);
+
   return (
     <>
+      <Header />
       <section className="shelter-section">
         <h1>Cat shelter</h1>
         <p className="preamble">
           Current inhabitants in the Cat Shelter. <br />
           If the house icon is enabled, the cat has found a home.
         </p>
+
+        <ul className="cat-list">
+          {cats &&
+            cats.map((cat) => {
+              return (
+                <li className="cat-list-item" key={cat.id}>
+                  <div>
+                    {" "}
+                    <i className="fas fa-paw" aria-hidden="true"></i>
+                    <span>{cat.catname}</span>
+                  </div>
+                </li>
+              );
+            })}
+        </ul>
       </section>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <p className="header-text">React course Lab 1</p>
+    </header>
+  );
+};
+
+export default Header;
